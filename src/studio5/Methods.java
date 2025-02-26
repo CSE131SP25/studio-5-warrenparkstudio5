19325a63fb45adf8cfad81c4fb87dc44c977cae6
jackjack <jackjack@localhost>
2025-02-26 14:10:59 -0600

    distance, bullseye, array, filled array

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -15,6 +15,7 @@ public class Methods {
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
+		distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
 		return distance;
@@ -34,17 +35,21 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
+		StdDraw.setPenColor(0, 109, 219);
+        StdDraw.filledCircle(x, y, radius*0.75);
 
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+        StdDraw.setPenColor(146, 0, 0);
+        StdDraw.filledCircle(x, y, radius*0.5);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+        StdDraw.setPenColor(255, 255, 109);
+        StdDraw.filledCircle(x, y, radius*0.25);
 		
 	}
 
@@ -75,6 +80,10 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
+		for(int i = 0; i < values.length; i++)
+		{
+			sum = sum + values[i];
+		}
 		
 		return sum;
 	}
@@ -87,7 +96,11 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int[length]; // FIXME: Create an array of the appropriate size
+		for(int i = 0; i < length; i++)
+		{
+			values[i] = value;
+		}
 		// TODO: Finish this method
 
 		
